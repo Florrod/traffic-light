1b0a17359df54c00d36370d0ba612f4437b12571
Flor Rodriguez <florrod23@gmail.com>
2020-05-15 20:04:49 +0000

    Subo el ejercicio completo

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,10 @@ import ReactDOM from "react-dom";
 import "bootstrap";
 
 //include your index.scss file into the bundle
-import "../styles/index.scss";
+import "../styles/intex.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { TrafficLight } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
